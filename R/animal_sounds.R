a5d59b7ae05d398ea73b84afdf166db192ca78eb
DJWilliamson <jamesandshantha@yahoo.com.au>
2026-06-01 18:39:26 +0800

    Add basic files

---
+++ R/animal_sounds.R
@@ -0,0 +1,18 @@
+#' Print the Sound an Animal Makes
+#'
+#' Takes an animal and the sound it makes and returns the combination.
+#'
+#' @param animal A string
+#' @param sound A string
+#'
+#' @returns A string with the animal and sound
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
+#'
+animal_sounds <- function(animal, sound) {
+    stopifnot(is.character(animal) & length(animal) ==1)
+    stopifnot(is.character(sound) & length(sound) ==1)
+    paste0("The ", animal, " goes ", sound, "!")
+}
