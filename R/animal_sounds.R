cd11a27aa374c4dce1a50ac1e20359ce4ce49d29
DJWilliamson <13284521+DJamesWilliamson@users.noreply.github>
2026-06-16 17:21:41 +0800

    Added check_arg function

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -13,26 +13,51 @@
 #' animal_sounds("dog", "woof")
 
 animal_sounds <- function(animal, sound) {
-	if (!rlang::is_character(animal, n = 1)) {
-		cli::cli_abort(
-			c(
-				"{.var animal} must be a single string",
-				"i" = "It was {.type {animal}} of {length(animal)} instead."
-            )
-		)
-	}
-	if (!rlang::is_character(sound, n = 1)) {
-		cli::cli_abort(
-		    c(
-		        "{.var sound} must be a single string",
-		        "i" = "It was {.type {sound}} of {length(sound)} instead."
-            )
-		)
-	}
-	paste0("The ", animal, " goes ", sound, "!")
+    check_arg(animal)
+    check_arg(sound)
+    paste0("The ", animal, " goes ", sound, "!")
 }
 
+# animal_sounds <- function(animal, sound) {
+# 	if (!rlang::is_character(animal, n = 1)) {
+# 		cli::cli_abort(
+# 			c(
+# 				"{.var animal} must be a single string",
+# 				"i" = "It was {.type {animal}} of {length(animal)} instead."
+#             )
+# 		)
+# 	}
+# 	if (!rlang::is_character(sound, n = 1)) {
+# 		cli::cli_abort(
+# 		    c(
+# 		        "{.var sound} must be a single string",
+# 		        "i" = "It was {.type {sound}} of {length(sound)} instead."
+#             )
+# 		)
+# 	}
+# 	paste0("The ", animal, " goes ", sound, "!")
+# }
+
+# check_arg <- function(arg, n = 1) {
+#     if (!rlang::is_character(arg, n = n)) {
+#         cli::cli_abort(
+#             c(
+#                 "{.var arg} must be a single string",
+#                 "i" = "It was {.type {arg}} of {length(arg)} instead."
+#             )
+#         )
+#     }
+# }
 
 
-
+check_arg <- function(arg, n = 1) {
+    if (!rlang::is_character(arg, n = n)) {
+        cli::cli_abort(
+            c(
+                "{.var {rlang::caller_arg(arg)}} must be a single string",
+                "i" = "It was {.type {arg}} of {length(arg)} instead."
+            )
+        )
+    }
+}
 
