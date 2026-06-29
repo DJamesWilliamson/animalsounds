47ac6afba226301318070a03d8a153eb80bd8dc1
DJWilliamson <13284521+DJamesWilliamson@users.noreply.github>
2026-06-29 17:45:58 +0800

    Changed #` to #'

--- R/data-farm_animals.R
+++ R/data-farm_animals.R
@@ -1,11 +1,11 @@
-#` Sounds Made by Animals
-#`
-#` A dataset of 3 animals and their corresponding sounds
-#`
-#` @format A data frame with 3 rows and 2 variables:
-#` \describe{
-#`      \item{animal}{The animal as a character string.}
-#`      \item{sound}{The sound made by the animal as a character string.}
-#`  }
+#' Sounds Made by Animals
+#'
+#' A dataset of 3 animals and their corresponding sounds
+#'
+#' @format A data frame with 3 rows and 2 variables:
+#' \describe{
+#'      \item{animal}{The animal as a character string.}
+#'      \item{sound}{The sound made by the animal as a character string.}
+#'  }
 "farm_animals"
 
--- R/data-letter_indices.R
+++ R/data-letter_indices.R
@@ -1,10 +1,10 @@
-#` Letters of the Alphabet with their Indices
-#`
-#` A dataset of lower case letters with their numeric indices from a = 1 to z = 26
-#`
-#` @format A data frame with 26 rows and 2 variables:
-#` \describe{
-#`      \item{letter}{The letter as a character string.}
-#`      \item{index}{The index as an integer.}
-#`  }
+#' Letters of the Alphabet with their Indices
+#'
+#' A dataset of lower case letters with their numeric indices from a = 1 to z = 26
+#'
+#' @format A data frame with 26 rows and 2 variables:
+#' \describe{
+#'      \item{letter}{The letter as a character string.}
+#'      \item{index}{The index as an integer.}
+#'  }
 "letter_indices"
