6eac4e84f93b56fa9263b22f3d508b2953f3b4ba
DJWilliamson <13284521+DJamesWilliamson@users.noreply.github>
2026-06-29 16:41:22 +0800

    Created documentation for datasets

--- R/data-letter_indices.R
+++ R/data-letter_indices.R
@@ -1,14 +1,3 @@
-#` Sounds Made by Animals
-#`
-#` A dataset of 3 animals and their corresponding sounds
-#`
-#` @format A data frame with 3 rows and 2 variables:
-#` \describe{
-#`      \item{animal}{The animal as a character string.}
-#`      \item{sound}{The sound made by the animal as a character string.}
-#`  }
-"farm_animals"
-
 #` Letters of the Alphabet with their Indices
 #`
 #` A dataset of lower case letters with their numeric indices from a = 1 to z = 26
