b580e7584d460e935d9fd22b828d2d19ea763b31
DJWilliamson <13284521+DJamesWilliamson@users.noreply.github>
2026-06-29 18:24:44 +0800

    Fixed parameter animal, stringr, example with cow argument

--- R/excited_sound.R
+++ R/excited_sound.R
@@ -4,19 +4,20 @@
 #'
 #' Repeat an animal sound in uppercase.
 #'
+#' @param animal A character string containing an animal.
 #' @param sound A character string containing an animal sound.
 #' @param times Number of times to repeat the sound.
 #'
 #' @return A character vector.
 #'
 #' @examples
-#' excited_sound("moo", 3)
+#' excited_sound("cow","moo", 3)
 #'
 #' @export
 excited_sound <- function(animal, sound, times = 3) {
 
     if (T) {
-        sound <- str_to_upper(sound)
+        sound <- stringr::str_to_upper(sound)
     }
 
     paste("The", animal, "says", paste(rep(sound, times), collapse = " "))
